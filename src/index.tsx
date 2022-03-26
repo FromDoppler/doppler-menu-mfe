9aca21cd0844aa169acc09e28368c4b4d0889c8c
Andrés Moschini <andresmoschini@gmail.com>
2022-03-26 18:28:03 -0300

    fix: render the menu in the right place

--- src/index.tsx
+++ src/index.tsx
@@ -4,12 +4,18 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+const targetElement = document.getElementById(
+  (window as any)["doppler-menu-mfe-configuration"]?.dopplerMenuElementId
 );
+
+if (targetElement) {
+  ReactDOM.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    targetElement
+  );
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
