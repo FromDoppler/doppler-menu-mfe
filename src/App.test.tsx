1c0d7ce525987b95232cc57f044a89c0db56dffb
bruno <brunoseguer@hotmail.com>
2022-04-14 09:11:28 -0300

    style: console.log on test removed

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,10 +1,8 @@
-import { prettyDOM, render, screen } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import App from "./App";
 
 test("renders Doppler Menu Micro-Frontend", () => {
-  const { container } = render(<App />);
-  console.log(prettyDOM(container));
-
+  render(<App />);
   const el = screen.getByText("Home");
   expect(el).toBeInTheDocument();
   expect(el.classList.toString()).toBe("active");
