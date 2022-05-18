1e1fdc6a035bca3479d37cb349ed3cb35c8b2d17
bruno <bseguer@makingsense.com>
2022-05-18 20:09:18 -0300

    feat: Nav active items style is based on current location

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
 import Header from "./components/Header";
-import { getUpdatedMenu, isInactiveSection } from "./utils";
 import headerData from "./headerData";
 import { HeaderMessages } from "./components/HeaderMessages";
 
 function App() {
-  const { pathname } = window.location;
+  const { pathname, search } = window.location;
   const { nav, notifications, emptyNotificationText, user, alert }: any =
     headerData;
 
@@ -12,8 +11,8 @@ function App() {
     <>
       <HeaderMessages alert={alert} user={user} />
       <Header
-        nav={getUpdatedMenu(pathname, nav)}
-        isInactiveSection={isInactiveSection(pathname)}
+        currentPath={`${pathname}${search}`}
+        nav={nav}
         notifications={notifications}
         emptyNotificationText={emptyNotificationText}
         user={user}
--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,83 +1,44 @@
 import { Notifications } from "./Notifications";
 import { UserMenu } from "./UserMenu";
 import { NavItem as INavItem, User } from "../headerData";
+import { useEffect, useState } from "react";
 
-interface SubNavItemsProp {
-  item: INavItem;
-  isInactiveSection: boolean;
+interface NavProp {
+  currentPath: string;
+  emptyNotificationText: string;
+  user: User;
+  nav: INavItem[];
+  notifications: string[];
+  openMenuHeader: () => void;
+  closeMenuHeader: () => void;
 }
 
 interface NavItemProp {
+  currentPath: string;
   item: INavItem;
-  isInactiveSection: boolean;
   openMenuHeader: () => void;
   closeMenuHeader: () => void;
 }
 
-interface NavProp {
-  nav: INavItem[];
-  user: User;
-  notifications: string[];
-  isInactiveSection: boolean;
-  emptyNotificationText: string;
-  openMenuHeader: () => void;
-  closeMenuHeader: () => void;
+interface SubNavProp {
+  currentPath: string;
+  isNavItemActive: boolean;
+  item: INavItem;
+  setNavItemActive: (value: boolean) => void;
 }
 
-const SubNavItems = ({ item, isInactiveSection }: SubNavItemsProp) => {
-  const { subNav } = item;
-  return (
-    <>
-      {subNav?.map(({ title, url, isSelected }) => {
-        return (
-          <li key={title}>
-            <a
-              className={isSelected && !isInactiveSection ? "active" : ""}
-              href={url}
-            >
-              {title}
-            </a>
-          </li>
-        );
-      })}
-    </>
-  );
-};
-
-const NavItem = ({
-  item,
-  isInactiveSection,
-  openMenuHeader,
-  closeMenuHeader,
-}: NavItemProp) => {
-  const { title, url, subNav = [], isSelected } = item;
-  const isActive = isSelected && !isInactiveSection;
-  const hasSubmenuItems = !!subNav.length;
-
-  return (
-    <li
-      key={title}
-      className={`${hasSubmenuItems ? "submenu-item" : ""}`}
-      onMouseEnter={() => hasSubmenuItems && openMenuHeader()}
-      onMouseLeave={() => hasSubmenuItems && closeMenuHeader()}
-    >
-      <a className={isActive ? "active" : ""} href={url}>
-        {title}
-      </a>
-      {
-        <ul className={`sub-menu ${isActive ? "open" : ""}`}>
-          <SubNavItems item={item} isInactiveSection={isInactiveSection} />
-        </ul>
-      }
-    </li>
-  );
-};
+interface SubNavItemProp {
+  currentPath: string;
+  url: string;
+  title: string;
+  setNavItemActive: (value: boolean) => void;
+}
 
 export const Nav = ({
+  currentPath,
   nav,
   user,
   notifications,
-  isInactiveSection,
   emptyNotificationText,
   openMenuHeader,
   closeMenuHeader,
@@ -87,12 +48,12 @@ export const Nav = ({
       <nav className="nav-left-main" aria-label="main nav">
         <div className="menu-main--container">
           <ul className="menu-main">
-            {nav.map((item) => {
+            {nav.map((item, index) => {
               return (
                 <NavItem
-                  key={`${item.idHTML}${item.title}`}
+                  currentPath={currentPath}
+                  key={index}
                   item={item}
-                  isInactiveSection={isInactiveSection}
                   openMenuHeader={openMenuHeader}
                   closeMenuHeader={closeMenuHeader}
                 />
@@ -120,5 +81,83 @@ export const Nav = ({
         </ul>
       </nav>
     </>
+  );
+};
+
+const NavItem = ({
+  currentPath,
+  item,
+  openMenuHeader,
+  closeMenuHeader,
+}: NavItemProp) => {
+  const { title, url, subNav = [] } = item;
+  const hasSubmenuItems = !!subNav.length;
+  const [isNavItemActive, setNavItemActive] = useState(
+    currentPath === item.url
+  );
+
+  useEffect(() => {
+    if (isNavItemActive && hasSubmenuItems) openMenuHeader();
+  }, [hasSubmenuItems, isNavItemActive, openMenuHeader]);
+
+  return (
+    <li
+      key={title}
+      className={`${hasSubmenuItems ? "submenu-item" : ""}`}
+      onMouseEnter={() => hasSubmenuItems && openMenuHeader()}
+      onMouseLeave={() => hasSubmenuItems && closeMenuHeader()}
+    >
+      <a className={isNavItemActive ? "active" : ""} href={url}>
+        {title}
+      </a>
+      <SubNav
+        isNavItemActive={isNavItemActive}
+        currentPath={currentPath}
+        item={item}
+        setNavItemActive={setNavItemActive}
+      />
+    </li>
+  );
+};
+
+const SubNav = ({
+  currentPath,
+  isNavItemActive,
+  item,
+  setNavItemActive,
+}: SubNavProp) => {
+  return (
+    <ul className={`sub-menu ${isNavItemActive ? "open" : ""}`}>
+      {item.subNav?.map(({ title, url }) => (
+        <SubNavItem
+          key={title}
+          currentPath={currentPath}
+          url={url}
+          title={title}
+          setNavItemActive={setNavItemActive}
+        />
+      ))}
+    </ul>
+  );
+};
+
+const SubNavItem = ({
+  url,
+  title,
+  currentPath,
+  setNavItemActive,
+}: SubNavItemProp) => {
+  const isActive = url === currentPath;
+
+  useEffect(() => {
+    if (isActive) setNavItemActive(true);
+  }, [isActive, setNavItemActive]);
+
+  return (
+    <li>
+      <a className={isActive ? "active" : ""} href={url}>
+        {title}
+      </a>
+    </li>
   );
 };
