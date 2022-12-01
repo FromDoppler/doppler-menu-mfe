0e22c4536d17652d0dcffbe39513ac45452ed470
Andres Moschini <andresmoschini@gmail.com>
2022-12-01 11:09:56 -0300

    fix: absolute URL for dashboard

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,7 +1,7 @@
 export const Logo = () => {
   return (
     <div className="logo" aria-label="company logo">
-      <a href="/dashboard">
+      <a href="https://app.fromdoppler.com/dashboard">
         <span
           className="ms-icon icon-doppler-logo"
           role="img"
