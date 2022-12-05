300122b50891de9e9e97d2bb8a8716f92f878ad5
Junior Campos <jcampos@makingsense.com>
2022-12-05 08:59:13 -0500

    feat: add beamer implementation

--- src/components/Notifications.test.tsx
+++ src/components/Notifications.test.tsx
@@ -1,8 +1,9 @@
 import { render } from "@testing-library/react";
+import { userData as defaultUser } from "../mocks/userMock";
 import { Notifications } from "./Notifications";
 
 describe("<Notifications />", () => {
   it("should display notifications", () => {
-    render(<Notifications />);
+    render(<Notifications user={defaultUser} />);
   });
 });
