7fe576dfa31edaba2c61bd62a2c558858fcbf6df
emoralesms <emorales@makingsense.com>
2026-08-18 11:57:25 -0300

    feat: create collaborator sections constants

---
+++ src/constants/collaboratorSections.ts
@@ -0,0 +1,12 @@
+import { CollaboratorSection } from "../model";
+
+export const COLLABORATOR_SECTIONS: Record<string, CollaboratorSection> = {
+  controlPanel: {
+    idSection: 4,
+    name: "ControlPanel",
+  },
+  dashboard: {
+    idSection: 14,
+    name: "Dashboard",
+  },
+};
